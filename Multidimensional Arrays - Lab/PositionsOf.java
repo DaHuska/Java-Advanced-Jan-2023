f20cf2aceb2a70b5b35c566d8ab75386424962cc
Alex Boyanov <aboyanov@abv.bg>
2023-01-23 23:38:15 +0200

    Solution for problem Positions Of

---
+++ Multidimensional Arrays - Lab/PositionsOf.java
@@ -0,0 +1,39 @@
+package com.company.Multidimensional_Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PositionsOf {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] dimensions = Arrays.stream(scan.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+
+        int rows = dimensions[0];
+        int cols = dimensions[1];
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int row = 0; row < matrix.length; row++) {
+            int[] arr = Arrays.stream(scan.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt).toArray();
+            matrix[row] = arr;
+        }
+
+        int searchNumber = Integer.parseInt(scan.nextLine());
+
+        boolean isFound = false;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (matrix[row][col] == searchNumber) {
+                    System.out.println(row + " " + col);
+                    isFound = true;
+                }
+            }
+        }
+
+        if (!isFound) {
+            System.out.println("not found");
+        }
+    }
+}
